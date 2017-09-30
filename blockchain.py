803a9ff777fd40e479021c8336ae5df2b37c921c
James Beake <jamesbeake@gmail.com>
2017-09-30 11:36:12 +1000

    MFV 1

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -94,3 +97,60 @@ class Blockchain(object):
     def last_block(self):
         # Returns the last block in the chain.
         return self.chain(-1)
+
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifer = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine, methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifer,
+        amount=1,
+    )
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/transaction/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing vlaues', 400
+    
+    index = blockchain.new_transaction(values['sender'], values['recepient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
