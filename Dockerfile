b215737246389fafac02c00706a27f2b81d858c7
James Beake <jamesbeake@gmail.com>
2017-09-30 10:51:02 +1000

    Before proof of work method

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM python:3-onbuild
 
-#COPY app /app
-#WORKDIR /app
-CMD ["python", "blockchain.py"]
 
 #ENV FLASK_APP=run.py
 #ENV FLASK_DEBUG=1
 #EXPOSE 5000
 
+CMD ["python", "blockchain.py"]
 
 #CMD [ "flask", "run", "--host=0.0.0.0"]
--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,66 @@
+import hashlib
+import json
+from time import time
+
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
     
     def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        """
+        Creates a new block and adds it to the blockchain
+        
+        :param proof: <int> The proof given by the Proff of Work Algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
     
-    def new_transaction(self):
-        # Creates a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to the list of transactions
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
     
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Create a SHA-256 hash of a Block
+
+        :param block <dict> Block
+        :return: <str>
+        """
     
     @property
     def last_block(self):
         # Returns the last block in the chain.
-        pass
-
+        return self.chain(-1)
